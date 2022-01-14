150ac9fbb8b00f654933819a507e5a9ece628ac4
luksinocencio <luksinocencio@gmail.com>
2022-01-14 20:05:28 -0300

    fix: schemes and dependencies

--- Bankey/BankeyUnitTests/BankeyUnitTests.swift
+++ Bankey/BankeyUnitTests/BankeyUnitTests.swift
@@ -13,17 +13,15 @@ class Test: XCTestCase {
     
     func testBreakDollarsIntoCents() throws {
         let result = formatter.breakIntoDollarsAndCents(929466.23)
-        XCTAssertEqual(result.0, "929,466")
+        XCTAssertEqual(result.0, "929.466")
         XCTAssertEqual(result.1, "23")
     }
     
-    // Challenge: You write
     func testDollarsFormatted() throws {
         let result = formatter.dollarsFormatted(929466.23)
         XCTAssertEqual(result, "$929,466.23")
     }
     
-    // Challenge: You write
     func testZeroDollarsFormatted() throws {
         let result = formatter.dollarsFormatted(0.00)
         XCTAssertEqual(result, "$0.00")
