d76158901c59cc0e1959eb44c7c5e51af883d3dc
luksinocencio <luksinocencio@gmail.com>
2022-01-19 00:45:34 -0300

    feat: Add shake sign in button if loggin fails

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -151,11 +151,6 @@ extension LoginViewController {
         }
         
         if username.isEmpty || password.isEmpty {
-            configureView(withMessage: "Username / Password should never be nil")
-            return
-        }
-        
-        if username.isEmpty || password.isEmpty {
             configureView(withMessage: "Username / password cannot be blank")
             return
         }
@@ -171,6 +166,18 @@ extension LoginViewController {
     private func configureView(withMessage message: String) {
         errorMessageLabel.isHidden = false
         errorMessageLabel.text = message
+        shakeButton()
+    }
+    
+    private func shakeButton() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.4
+        
+        animation.isAdditive = true
+        signInButton.layer.add(animation, forKey: "shake")
     }
 }
 
