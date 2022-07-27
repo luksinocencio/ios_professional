cd5b00d7e443c059c823c8925c773617a4f7bf98
luksinocencio <luksinocencio@gmail.com>
2022-07-26 22:55:59 -0300

    feat: create new password project

---
+++ Password/Password/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Lucas Inocencio on 26/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
